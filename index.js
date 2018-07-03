399a51ac7f698aeb256edd3805d16a1a3a0076de
dbarabashdev <dbarabashdev@gmail.com>
2018-07-03 22:41:16 +0300

    Added a POST Route To Express And Parse the Body

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
 
-app.get('/status', (req, res) => {
-  const localTime = new Date().toLocaleTimeString();
-  res.status(200).send(`Server time ${localTime}`);
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  const user = username;
+
+  res.status(200).send(`You logged in with ${user}`);
 });
 
 app.get('*', (req, res) => {
