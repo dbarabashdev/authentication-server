bad5167655325aa4793aa7b685d66502bb6fa316
dbarabashdev <dbarabashdev@gmail.com>
2018-07-03 23:00:11 +0300

    Provide Users With A JSON Web Token

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
 
+const users = [
+  { id: 1, username: 'admin', password: 'admin' },
+  { id: 2, username: 'quest', password: 'quest' }
+];
+
 app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
-  const { username } = req.body;
-  const user = username;
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).send('You need a username and password');
+    return;
+  }
+
+  const user = users.find(u => {
+    return u.username === username && u.password === password;
+  });
+
+  if (!user) {
+    res.status(401).send('User not found');
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      username: user.username
+    },
+    'secretkey',
+    { expiresIn: '3 hours' }
+  );
+
+  res.status(200).send({ access_token: token });
 
   res.status(200).send(`You logged in with ${user}`);
 });
